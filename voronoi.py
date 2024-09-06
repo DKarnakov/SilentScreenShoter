b181d6496641347ad974bfb4aaade28516acd07c
DKarnakov <dmitriy@karnakov.com>
2024-09-06 15:22:22 +0300

    06-09-24

--- voronoi.py
+++ voronoi.py
@@ -8,34 +8,35 @@ class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
 
-        self.geometry('600x600')
+        self.geometry('1000x600')
         self.canvas = tk.Canvas(self, cursor='cross', highlightthickness=0)
         self.canvas.pack(side='top', fill='both', expand=True)
 
         self.coords = []
         self.start_point = []
         self.polygons = []
+        self.is_construct = False
 
         self.bind('<Escape>', lambda e: self.destroy())
-        self.canvas.bind('<ButtonPress-1>', lambda e: self._create_rect(e))
-        self.canvas.bind('<B1-Motion>', lambda e: self._redraw_rect(e))
-        self.canvas.bind('<ButtonRelease-1>', lambda e: self._relax_rect(e))
+        self.canvas.bind('<ButtonPress-1>', lambda e: self._start_drawing(e))
+        self.canvas.bind('<B1-Motion>', lambda e: self._redraw_mosaic(e))
+        self.canvas.bind('<ButtonRelease-1>', lambda e: self._offset_mosaic(e))
 
-    def _create_rect(self, event):
+    def _start_drawing(self, event):
         self.start_point = [event.x, event.y]
 
-    def _redraw_rect(self, event, relax=False):
+    def _redraw_mosaic(self, event, relax=False):
         self.coords = [min(self.start_point[0], event.x), min(self.start_point[1], event.y),
                        max(self.start_point[0], event.x), max(self.start_point[1], event.y)]
         width = self.coords[2] - self.coords[0]
         height = self.coords[3] - self.coords[1]
-        dens = 50
-        np.random.seed(1801)
-        gen_points = np.random.rand(max(4, (width * height) // dens ** 2), 2)
+        dens = 30
         self.canvas.delete('all')
         if relax:
             points = np.array([list(*polygon.centroid.coords) for polygon in self.polygons])
         else:
+            np.random.seed(1801)
+            gen_points = np.random.rand(max(4, (width * height) // dens ** 2), 2)
             points = np.array([[point[0]*width+self.coords[0], point[1]*height+self.coords[1]] for point in gen_points])
         ext_points = np.array([[self.coords[0]-width*3, self.coords[1]-height*3],
                                [self.coords[2]+width*3, self.coords[1]-height*3],
@@ -57,10 +58,20 @@ class Application(tk.Tk):
         except QhullError:
             ...
 
-    def _relax_rect(self, event):
-        for _ in range(10):
-            self.update()
-            self._redraw_rect(event, relax=True)
+    def _offset_mosaic(self, event):
+        while not self.is_construct:
+            self.is_construct = True
+            for polygon in self.polygons:
+                polygon_offset = polygon.buffer(-7)
+                if polygon_offset.is_empty:
+                    self.is_construct = False
+                    self._redraw_mosaic(event, relax=True)
+                    self.update()
+                    break
+                else:
+                    self.canvas.create_polygon(list(polygon_offset.exterior.coords),
+                                               outline='grey25', fill='', smooth=True)
+        self.is_construct = False
 
 
 if __name__ == '__main__':
